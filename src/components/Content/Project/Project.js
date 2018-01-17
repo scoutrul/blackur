728587d2e887b8c9f223adbbb2eaf4facc329a59
scoutrul <scoutrul@mail.ru>
2018-01-18 02:16:31 +0300

    logo offset scroll OK

--- src/components/Content/Project/Project.js
+++ src/components/Content/Project/Project.js
@@ -2,10 +2,40 @@ import React, { Component } from "react";
 import "./project.scss";
 
 export default class extends Component {
+	state ={
+		screenHeight: null
+	};
+	
+	_bindResize = element => {
+		this.setState({
+			screenHeight: window.innerHeight
+		});
+		window.addEventListener(
+			"resize",
+			() => {
+				let resizeTimeout;
+				if (!resizeTimeout) {
+					console.log('resizing')
+					resizeTimeout = setTimeout(() => {
+						resizeTimeout = null;
+						this.setState({
+							screenHeight: window.innerHeight
+						});
+					}, 66);
+				}
+			},
+			false
+		);
+	};
+	
+	componentDidMount() {
+		this._bindResize(document.getElementsByTagName('body')[0]);
+	}
+	
 	render() {
 		return (
 			<div className={"project"}>
-				<div className={"one"} id={"one"}>
+				<div className={"one"} id={"one"} style={{height: this.state.screenHeight - 100}}>
 					{}
 				</div>
 				<div className={"two"}>{}</div>
--- src/components/Header/headerLogo.js
+++ src/components/Header/headerLogo.js
@@ -27,7 +27,7 @@ export default class extends Component {
 	
 	_bindResize = element => {
 		this.setState({
-			screenHeight: element.clientHeight
+			screenHeight: element.getBoundingClientRect().height
 		});
 		window.addEventListener(
 			"resize",
@@ -37,9 +37,8 @@ export default class extends Component {
 					console.log('resizing')
 					resizeTimeout = setTimeout(() => {
 						resizeTimeout = null;
-						
 						this.setState({
-							screenHeight: element.clientHeight
+							screenHeight: element.getBoundingClientRect().height
 						});
 					}, 66);
 				}
@@ -49,9 +48,8 @@ export default class extends Component {
 	};
 	
 	componentDidMount() {
-		const element = document.getElementsByClassName("mainFrame")[0] ;
-		this._bindScroll(element);
-		this._bindResize(element);
+		this._bindScroll(document.getElementsByClassName("mainFrame")[0]);
+		this._bindResize(document.getElementsByTagName('body')[0]);
 	}
 	
 	componentDidUpdate() {
@@ -60,54 +58,66 @@ export default class extends Component {
 	
 	render() {
 		
-		// работать с нормальным svg с понятным размером
-		// положение скролла минус размер блока минус размер логотипа
-		// тогда применять к маске эту разницу
-		// пока не настанет положение скролла > размер блока + размер логотипа
-		
-		const logoSize = 54;
-		const padding = 109;
+		let [svgW, svgH] = [48.5, 54];
+		const logoSize = svgH;
+		const padding = 80 + 46;
 		const topScroll = this.state.topScroll;
 		const MaskHeight = this.state.screenHeight;
 		
-		let viewLimit = ((topScroll > (MaskHeight - logoSize*1.5) - padding ) && (topScroll < (MaskHeight + logoSize*1.5) - padding));
+		let stopper1 = MaskHeight - (logoSize * 2) - padding;
+		let stopper2 = stopper1 + logoSize*2
+		let viewLimit = topScroll >= stopper1 && topScroll <= stopper2;
 		
-		let koef = viewLimit && topScroll - MaskHeight + logoSize*1.5 + padding ;
-		
-		let initWhite =  (topScroll > (MaskHeight + logoSize*1.5) - padding-10 ) ? 100 : -koef || 0;
-		let initBlack =  0;
-		
-		console.log(topScroll, koef)
-		
-		let [svgW, svgH] = [48.5, 54];
-		const svgSource = `
-	<svg
-		viewBox="0 0 ${svgW} ${svgH}"
-		version="1.1"
-		width=${svgW} height=${svgH}
-		xmlns="http://www.w3.org/2000/svg">
-			<defs>
-				<clipPath id="logo-white">
-					<rect x="0" y=${initWhite} width=${svgW} height=${svgH}></rect>
-				</clipPath>
-				<clipPath id="logo-black">
-					<rect x="0" y=${initBlack} width=${svgW} height=${svgH}></rect>
-				</clipPath>
-			</defs>
+		let koef = viewLimit && topScroll - (MaskHeight - padding - logoSize*2);
 
-        <g id="black" clip-path="url(#logo-black)">
-            <path id="path01" d="M40,26.4c3.8-2.6,6.3-7,6.3-11.9c0-8-6.5-14.4-14.4-14.4H0v54h34.1c8,0,14.4-6.5,14.4-14.4
-	C48.5,33.7,45.1,28.6,40,26.4z" stroke="none" fill="#131517" ></path>
-        </g>
-        <g id="white" clip-path="url(#logo-white)">
-            <path id="path02" d="M40,26.4c3.8-2.6,6.3-7,6.3-11.9c0-8-6.5-14.4-14.4-14.4H0v54h34.1c8,0,14.4-6.5,14.4-14.4
-	C48.5,33.7,45.1,28.6,40,26.4z" stroke="none" fill="#FFFFFF" ></path>
-        </g>
-	</svg>`;
+		let whiteMask = (topScroll > stopper2) ? svgH : -koef || 0;
+		let initBlack = 0;
+		
+		
+		const svgSource = `
+			<svg
+				viewBox="0 0 ${svgW} ${svgH}"
+				version="1.1"
+				width=${svgW} height=${svgH}
+				xmlns="http://www.w3.org/2000/svg">
+					<defs>
+						<clipPath id="logo-white">
+							<rect x="0" y=${whiteMask} width=${svgW} height=${svgH} ></rect>
+						</clipPath>
+						<clipPath id="logo-black">
+							<rect x="0" y=${0} width=${svgW} height=${svgH}></rect>
+						</clipPath>
+					</defs>
+				<g id="black" clip-path="url(#logo-black)">
+					<path id="path01" d="M40,26.4c3.8-2.6,6.3-7,6.3-11.9c0-8-6.5-14.4-14.4-14.4H0v54h34.1c8,0,14.4-6.5,14.4-14.4
+			C48.5,33.7,45.1,28.6,40,26.4z" stroke="none" fill="#131517" ></path>
+				</g>
+				<g id="white" clip-path="url(#logo-white)">
+					<path id="path02" d="M40,26.4c3.8-2.6,6.3-7,6.3-11.9c0-8-6.5-14.4-14.4-14.4H0v54h34.1c8,0,14.4-6.5,14.4-14.4
+			C48.5,33.7,45.1,28.6,40,26.4z" stroke="none" fill="#FFFFFF" ></path>
+				</g>
+			</svg>`;
 		
 		return (
 			<div className={"headerLogo"}>
-				<InlineSVG src={svgSource} style={{ position: 'absolute' }}/>
+				<div style={{position:'fixed', top: 100, left: 700 }}>
+					<ul>
+						<li>stopper1 {stopper1}</li>
+						<li>stopper2 {stopper2}</li>
+						<li>koef {koef}</li>
+						<li>topScroll {topScroll}</li>
+						<li>whiteMask {whiteMask}</li>
+					</ul>
+				</div>
+				
+				<div className={'line'} style={{ top: stopper1 }}>stopper1 {stopper1} ___________</div>
+				<div className={'line'} style={{ top: stopper2 }}>stopper2 {stopper2} ___________</div>
+				
+				<div className={'line'} style={{ top: koef }}>____________________koef{koef}</div>
+				<div className={'line'}
+					 style={{ top: topScroll }}>_____________________________ {viewLimit ? 'true' : 'false'}_______TopScroll {topScroll},</div>
+				
+				<InlineSVG src={svgSource} id={'svgLogo'}/>
 			</div>
 		);
 	}
