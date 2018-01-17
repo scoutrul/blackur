146ef0161146e0def18fa5a898e2bdc71481d7dd
scoutrul <scoutrul@mail.ru>
2018-01-17 18:39:48 +0300

    gh-pages update

---
+++ src/components/Content/Project/Project.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./project.scss";
+
+export default class extends Component {
+	render() {
+		return (
+			<div className={"project"}>
+				<div className={"one"} id={"one"}>
+					{}
+				</div>
+				<div className={"two"}>{}</div>
+				<div className={"three"}>{}</div>
+			</div>
+		);
+	}
+}
