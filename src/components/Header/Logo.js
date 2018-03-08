acf19a0e2288b1fe677e16210f6474f303999971
scoutrul <scoutrul@mail.ru>
2018-03-08 17:21:26 +0300

    logo smooth color changer

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,50 +1,55 @@
 import React, { Component } from 'react';
-import Appear from '../HOC/Appear'
-import AnimatedLink from '../HOC/AnimatedLink'
-import PropTypes from 'prop-types'
+import Appear from '../HOC/Appear';
+import AnimatedLink from '../HOC/AnimatedLink';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import './logo.scss';
 
-@connect(state => {
+@connect((state) => {
 	return {
 		changeColorBool: state.Animations.changeColorinWork
-	}
+	};
 })
 @Appear
 export default class extends Component {
-
 	static propTypes = {
-		MovingActions: PropTypes.object,
-	}
+		MovingActions: PropTypes.object
+	};
 
 	render() {
-
 		let { viewBoxHeight, divHeight, divTopOffset, scrollTop, actionBlock } = this.props.MovingActions;
 
 		let move = actionBlock ? scrollTop - (viewBoxHeight - divTopOffset - divHeight) : 0;
 
 		let changeColor = this.props.changeColorBool ? '0' : !actionBlock && '0';
 
-		return <AnimatedLink to={'/'}>
-			<div className={`logo ${this.props.AnimationCss}`}>
-				<div className={'logos'}>
-					<div className="white"
-						style={{ transform: `translateY(${!this.props.changeColorBool ? -move : 0}px)` }}>
-						<img src={'images/logo_blackur.svg'}
-							alt={'Blackur logo'}
-							style={{ transform: `translateY(${!this.props.changeColorBool ? move : 0}px)` }} />
-					</div>
-					<div className="black"
-						style={{ transform: `translateY(${move}px)`, opacity: changeColor}}>
-						<img src={'images/logo_blackur.svg'}
-							alt={'Blackur logo'}
-							style={{ transform: `translateY(${-move}px)` }} />
+		return (
+			<AnimatedLink to={'/'}>
+				<div className={`logo ${this.props.AnimationCss}`}>
+					<div className={'logos'}>
+					 	<div className="to_white" style={{ opacity: this.props.changeColorBool ? 1 : 0 }}>
+							<img src={'images/logo_blackur.svg'} alt={'Blackur logo'} />
+						</div>
+						<div className="white" style={{ transform: `translate3D(0, ${-move}px ,0)` }}>
+							<img
+								src={'images/logo_blackur.svg'}
+								alt={'Blackur logo'}
+								style={{ transform: `translate3D(0, ${!this.props.changeColorBool ? move : 0}px ,0)` }}
+							/>
+						</div>
+						<div className="black" style={{ transform: `translate3D(0, ${move}px ,0)` }}>
+							<img
+								src={'images/logo_blackur.svg'}
+								alt={'Blackur logo'}
+								style={{
+									transform: `translate3D(0, ${-move}px ,0)`
+								}}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
-		</AnimatedLink>
-
+			</AnimatedLink>
+		);
 	}
-
 }
